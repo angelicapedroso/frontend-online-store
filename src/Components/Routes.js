c87ee6fc24d13d92ca8e34519fc78cdb2d715be8
Fabiana Lopes Santana <fabianalopes93@outlook.com>
2021-11-18 16:01:55 -0300

    Requisito um

---
+++ src/Components/Routes.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Cart from '../../pages/Cart';
+
+class Routes extends Component {
+  render() {
+    return (
+      <div>
+				<Switch>
+				<Router exact path="/" component={ Cart } />
+        <Router exact path="/Page1" component={ Page1 } />
+				</Switch>
+      </div>
+        );
+    }
+}
+
+export default Routes;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories'; // boa prática
+  return fetch(url) // fazendo requisição
+  .then((data) => data.json()); //retorna uma promise, queremos API completa
+  
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`; // template literals pq recebe os parametros. Coloca  os param. dentro da API.
+  return fetch(url)
+  .then((data) => data.json ());
 }

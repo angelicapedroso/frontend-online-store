4c9e80f2eafcc10e669247294fbed33d50451d7f
Fabiana Lopes Santana <fabianalopes93@outlook.com>
2021-11-18 16:24:38 -0300

    requisito um

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,11 @@
 export async function getCategories() {
-  const url = 'https://api.mercadolibre.com/sites/MLB/categories'; // boa prática
-  return fetch(url) // fazendo requisição
-  .then((data) => data.json()); //retorna uma promise, queremos API completa
-  
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  return fetch(url)
+    .then((data) => data.json());
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`; // template literals pq recebe os parametros. Coloca  os param. dentro da API.
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   return fetch(url)
-  .then((data) => data.json ());
+    .then((data) => data.json());
 }

35e1318263394a9b5d5e9d9f9dcdf942f80c43eb
aparecidag365 <mgegoulart365@gmail.com>
2021-11-19 18:02:31 -0300

    adicionando requisito 4

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,19 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
-class Cart extends Component {
+class Cart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() {
+    getCategories().then((product) => {
+      this.setState({
+        categories: product,
+      });
+    });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
         <div>
+          {categories.map((category) => (
+            <section key={ category.id }>
+              <Link to={ `/${category.id}` } data-testid="category" />
+            </section>
+          ))}
+        </div>
+        <div>
           <button type="button">
             <Link to="/CartPage" data-testid="shopping-cart-button" />
           </button>
         </div>
-        ;
       </>
     );
   }

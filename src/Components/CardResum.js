f2cbb71fc3fd77ca7980e74c2d161dbd5e0d389c
Angelica <angelicapedroso05@gmail.com>
2021-11-25 14:47:22 -0300

    requisito 8 - incompleto

--- src/Components/CardResum.js
+++ src/Components/CardResum.js
@@ -5,18 +5,26 @@ import { Link } from 'react-router-dom';
 
 class CardResum extends React.Component {
   render() {
-    const { product: { title, thumbnail, price, id } } = this.props;
+    const { product: { title, thumbnail, price, id }, addProductCart, product } = this.props;
     return (
-      <Link
-        data-testid="product-detail-link"
-        to={ { pathname: `/productDetails/${id}` } }
-      >
-        <div data-testid="product">
-          <h3>{title}</h3>
-          <img src={ thumbnail } alt={ title } />
-          <p>{price}</p>
-        </div>
-      </Link>
+      <div>
+        <Link
+          data-testid="product-detail-link"
+          to={ { pathname: `/productDetails/${ id }` } }
+        >
+          <div data-testid="product">
+            <h3 data-testid="shopping-cart-product-name">{ title }</h3>
+            <img src={ thumbnail } alt={ title } />
+            <p>{ price }</p>
+          </div>
+        </Link>
+        <input
+          data-testid="product-add-to-cart"
+          type="button"
+          value="Adicionar ao carrinho"
+          onClick={ () => addProductCart(product) }
+        />
+      </div>
     );
   }
 }
@@ -28,6 +36,7 @@ CardResum.propTypes = {
     price: PropTypes.number,
     id: PropTypes.string,
   }).isRequired,
+  addProductCart: PropTypes.func.isRequired,
 };
 
 export default CardResum;
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class CartPage extends Component {
+  constructor(props) {
+    super(props);
+    const { state: { products } } = props.location;
+    this.state = {
+      cardProduct: products,
+    };
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>

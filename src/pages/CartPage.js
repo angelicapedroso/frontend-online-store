ffc370d19f9b2fa6abf3e9761a1e2d1c0ef8e02b
Angelica <angelicapedroso05@gmail.com>
2021-11-26 00:00:51 -0300

    renderiza produtos no carrinho - incompleto e com erro

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class CartPage extends Component {
   constructor(props) {
     super(props);
-    const { state: { products } } = props.location;
+    // const { state: { products } } = props.location;
+    // console.log(products);
+    const { location: { state: { products } } } = this.props;
     this.state = {
       cardProduct: products,
     };
+    console.log(products);
   }
 
   render() {
-    console.log(this.state);
+    const { cardProduct } = this.state;
     return (
-      <div>
-        <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
-      </div>
+
+      (
+        cardProduct.length <= 0
+          ? <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
+          : (<section>
+            {
+              cardProduct.map((product) => (
+                <h3 key={ product.id }>
+                  { product.title }
+                  <img src={ product.thumbnail } alt={ product.title } />
+                  <p>{ product.price }</p>
+                </h3>
+              ))
+            }
+             </section>
+          )
+      )
     );
   }
 }
+
+CartPage.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.string,
+    products: PropTypes.string,
+  }).isRequired,
+};
+
+// props.location.state;
 
 export default CartPage;
